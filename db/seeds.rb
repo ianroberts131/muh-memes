499c89637319f967de3817f0c052b4fdf2971941
ianroberts131 <ianroberts131@gmail.com>
2017-02-26 03:36:28 +0000

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Ian Roberts",
+             email: "ianroberts131@gmail.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
+             
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@user.test"
+  password = "password"
+  
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
