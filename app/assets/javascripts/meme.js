de613a61084bd0f1310881535774d4c0814b7919
ianroberts131 <ianroberts131@gmail.com>
2017-06-03 21:00:10 +0000

    increase timeout to 300

--- app/assets/javascripts/meme.js
+++ app/assets/javascripts/meme.js
@@ -239,7 +239,7 @@ $(document).on("turbolinks:load", function() {
         document.getElementById('top-font-style-select').value = "Impact-Outline";
         textBoxTop.lockMovementX = true;
         canvas.add(textBoxTop);
-      }, 250);
+      }, 300);
       
   }
   
@@ -262,7 +262,7 @@ $(document).on("turbolinks:load", function() {
       document.getElementById('bottom-font-style-select').value = "Impact-Outline";
       textBoxBottom.lockMovementX = true;
       canvas.add(textBoxBottom);
-    }, 250);
+    }, 300);
   }
 
   // Toggle top bold option
