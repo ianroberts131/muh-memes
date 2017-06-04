ea78958c1a299b2d669c9cc48c0767dff94153d1
ianroberts131 <ianroberts131@gmail.com>
2017-06-04 20:28:30 +0000

    Finish homepage meme filters

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,13 @@
 class Favorite < ApplicationRecord
-  belongs_to :user
-  belongs_to :favorited, polymorphic: true
+  belongs_to    :user
+  belongs_to    :favorited, polymorphic: true
+  after_save    :update_favorites_count_index
+  after_destroy :update_favorites_count_index
 end
+
+private
+
+  def update_favorites_count_index
+    Sunspot.index!(favorited)
+  end
+
